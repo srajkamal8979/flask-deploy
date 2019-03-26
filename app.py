8f8f052806037d13e6f1a35eb969743c0646914a
Rajkamal8979 <srajkamal84@gmail.com>
2019-03-26 13:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,85 +28,83 @@ def hello_world():
     return 'Hello World!'
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    speech_text=[]
+    #peech_text=[]
     con = sql.connect("database.db")
     cur=con.cursor()
     if con:
         print ("Connected database successfully")
     else:
         print("connect failed")
-    result=cur.execute("select * from mytable")
-    rows=result.fetchall()
-    for row in rows:
-        speech_text.append(row[1])
-    speechtext=list(set(speech_text))
-    speech="The candidates are :  " + "{}.".format(','.join(speechtext))
-    print(speech)
-    op={'speech':speech}
-    res=json.dumps(op,indent=4)
-    r=make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-    #return result
-       
-#     req = request.get_json(silent=True, force=True)
-
-#     print("Request:")
-#     print(json.dumps(req, indent=4))
-
-#     res = makeWebhookResult(req)
-
-#     res = json.dumps(res, indent=4)
-#     print(res)
-#     r = make_response(res)
+#     result=cur.execute("select * from mytable")
+#     rows=result.fetchall()
+#     for row in rows:
+#         speech_text.append(row[1])
+#     speechtext=list(set(speech_text))
+#     speech="The candidates are :  " + "{}.".format(','.join(speechtext))
+#     print(speech)
+#     op={'speech':speech}
+#     res=json.dumps(op,indent=4)
+#     r=make_response(res)
 #     r.headers['Content-Type'] = 'application/json'
 #     return r
+    #return result
+       
+    req = request.get_json(silent=True, force=True)
 
-# def makeWebhookResult(req):
-#     speech_text=[]
-#     test_code=[]
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    speech_text=[]
+    test_code=[]
+    #print("this is mine"+ req.get("result").get("action"))
+    result = req.get("result")
+    print(result)
+    parameters = result.get("parameters")
+    skillset = parameters.get("skills-list")
+#     cur.prepare('select * from mytable where skill = :skills-list')
+#     cur.execute(None, {'skill': skillset})
+    cur.execute('select * from mytable where skill = ?',[skillset]')
+    rows = cur.fetchall()
+    for row in rows:
+        speech_text.append(row[0])
     
-    
-#     print("this is mine"+ req.get("result").get("action"))
-#     result = req.get("result")
-#     print(result)
-#     parameters = result.get("parameters")
-#     skillset = parameters.get("skills-list")
-# #     cur.prepare('select * from mytable where skill = :skills-list')
-# #     cur.execute(None, {'skill': skillset})
-#     cur.execute('select * from mytable where skill = :skillset')
-#     rows = cur.fetchall()
-#     for row in rows:
-#         speech_text.append(row[0])
-    
-# #     with sql.connect("database.db") as con:
-# #         cur = con.cursor()
-# #         con.row_factory = sql.Row
-# #         cur = con.cursor()
-# #         cur.execute("select * from mytable where city= ?",[zone])
-# #         rows = cur.fetchall()
-# #         for row in rows:
-# #             speech_text.append(row[0])
-#     con.close()
-#     speechtext=list(set(speech_text))
-#     print(speechtext)
-#     speech="The candidates for skill  "+skillset+ "  are :  " + "{}.".format(','.join(speechtext))        
+#     with sql.connect("database.db") as con:
+#         cur = con.cursor()
+#         con.row_factory = sql.Row
+#         cur = con.cursor()
+#         cur.execute("select * from mytable where city= ?",[zone])
+#         rows = cur.fetchall()
+#         for row in rows:
+#             speech_text.append(row[0])
+    con.close()
+    speechtext=list(set(speech_text))
+    print(speechtext)
+    speech="The candidates for skill  "+skillset+ "  are :  " + "{}.".format(','.join(speechtext))        
             
-#         #print(row[0],row[1],row[2],row[3])
-#     #print(rows)
+        #print(row[0],row[1],row[2],row[3])
+    #print(rows)
 
-#     #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
-#     print("Response:")
-#     print(speech)
+    print("Response:")
+    print(speech)
 
-#     return {
-#         "speech": speech,
-#         "displayText": "It's done",
-#         #"data": {},
-#         # "contextOut": [],
-#         "source": "apiai-onlinestore-shipping"
-#     }
+    return {
+        "speech": speech,
+        "displayText": "It's done",
+        #"data": {},
+        # "contextOut": [],
+        "source": "skills database"
+    }
 
 
 if __name__ == '__main__':
